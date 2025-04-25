1ee37e56c945b52bee724db32d72b7f594550396
Bryan Liu <bryanliuh@gmail.com>
2025-04-25 00:08:10 -0700

    updated baseline Gshare to be ~128KB

--- src/my_predictor.h
+++ src/my_predictor.h
@@ -12,7 +12,7 @@ public:
 class my_predictor : public branch_predictor {
 public:
 #define HISTORY_LENGTH	15
-#define TABLE_BITS	15
+#define TABLE_BITS	20
 	my_update u;
 	branch_info bi;
 	unsigned int history;
