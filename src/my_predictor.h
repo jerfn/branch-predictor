e2e60d3a33ffa3ca92c13a5721c7144d8338fd5c
jerfn <jfang1209@gmail.com>
2025-04-25 12:54:02 -0700

    worse perfomance than normal perceptron

--- src/my_predictor.h
+++ src/my_predictor.h
@@ -12,36 +12,46 @@ public:
 	unsigned int index;
 };
 
+#define NUM_HASHES 16 // number of feature hashes
+#define TABLE_SIZE 2048 // number of entries in each weight table
 #define HISTORY_LENGTH  64
-#define NUM_PERCEPTRONS 2048 // number of different perceptrons (1 per unique branch)
 #define MIN_WEIGHT -128
 #define MAX_WEIGHT 127
-constexpr int THETA = (int) (1.93 * HISTORY_LENGTH + 14); // Training threshold
+// constexpr int THETA = (int) (1.93 * HISTORY_LENGTH + 14); // Training threshold
+constexpr int THETA = 40;
 
 class perceptron {
 public:
-	int8_t weights[HISTORY_LENGTH]; // using int8_t for weights, -128 to 127
+	int8_t W[NUM_HASHES][TABLE_SIZE]; // weight banks
 	int bias;
 
 	perceptron () {
-		for (int i = 0; i < HISTORY_LENGTH; i++)
-			weights[i] = 0;
+		for (int i = 0; i < NUM_HASHES; i++) {
+			for (int j = 0; j < TABLE_SIZE; j++) {
+				W[i][j] = 0;
+			}
+		}
 		bias = 0;
 	}
 
-	int predict(const std::bitset<HISTORY_LENGTH>& ghist) const {
+	int hash(int i, unsigned int pc, const std::bitset<HISTORY_LENGTH>& ghist) const {
+		uint64_t ghr = ghist.to_ullong();
+		return ((pc >> i) ^ (ghist.to_ullong() >> (3 * i)) ^ i * 0x5bd1e995) & (TABLE_SIZE - 1); // 5.618
+    }
+
+	int predict(unsigned int pc, const std::bitset<HISTORY_LENGTH>& ghist) const {
         int y = bias;
-        for (int i = 0; i < HISTORY_LENGTH; i++) {
-			int input = ghist[i] ? 1 : -1;
-            y += weights[i] * input;
+        for (int i = 0; i < NUM_HASHES; i++) {
+			// int input = ghist[i] ? 1 : -1;
+            y += W[i][hash(i, pc, ghist)];
         }
         return y;
 	}
 	
-	void train(const std::bitset<HISTORY_LENGTH>& ghist, bool taken) {
-		for (int i = 0; i < HISTORY_LENGTH; i++) {
-			int correction = (ghist[i] == taken ? 1 : -1); // increment if branch outcome agrees w/ ith history input, else decrement
-			weights[i] = std::max(MIN_WEIGHT, std::min(MAX_WEIGHT, weights[i] + correction));
+	void train(unsigned int pc, const std::bitset<HISTORY_LENGTH>& ghist, bool taken) {
+		for (int i = 0; i < NUM_HASHES; i++) {
+			int correction = taken ? 1 : -1;
+			W[i][hash(i, pc, ghist)] = std::max(MIN_WEIGHT, std::min(MAX_WEIGHT, W[i][hash(i, pc, ghist)] + correction));
 		}
 		bias = std::max(MIN_WEIGHT, std::min(MAX_WEIGHT, bias + (taken ? 1 : -1)));
 	}
@@ -49,40 +59,24 @@ public:
 
 class my_predictor : public branch_predictor {
 public:
-#define PC_HIST_LENGTH 2
 
 	my_update u;
 	branch_info bi;
 
-	std::bitset<HISTORY_LENGTH> ghist; // Global branch history
-	std::vector<perceptron> table;
+	perceptron p;
 
-	unsigned int pc_hist[PC_HIST_LENGTH]; // recent PCs
+	std::bitset<HISTORY_LENGTH> ghist; // Global branch history
 
 	my_predictor (void) {
-		table.resize(NUM_PERCEPTRONS);
 
-		for (int i = 0; i < PC_HIST_LENGTH; i++)
-			pc_hist[i] = 0;
 	}
 
 	branch_update *predict (branch_info & b) {
 		bi = b;
 		if (b.br_flags & BR_CONDITIONAL) { // if conditional branch
-			// u.index = b.address % NUM_PERCEPTRONS;
-			u.index = (b.address ^ ghist.to_ullong()) % NUM_PERCEPTRONS; // hash with ghist
 
-			// unsigned int hash = pc_hist[0];
-			// for (int i = 1; i < PC_HIST_LENGTH; i++)
-			// 	hash ^= pc_hist[i];
-			// u.index = (b.address ^ hash ^ ghist.to_ullong()) % NUM_PERCEPTRONS; // hash with recent PCs (path) and ghist
+			u.direction_prediction (p.predict(b.address, ghist) >= 0);
 
-			perceptron& p = table[u.index];
-			u.direction_prediction (p.predict(ghist) >= 0);
-
-			for (int i = PC_HIST_LENGTH - 1; i > 0; i--)
-				pc_hist[i] = pc_hist[i - 1];
-			pc_hist[0] = b.address;
 		} else {
 			u.direction_prediction (true);
 		}
@@ -92,11 +86,10 @@ public:
 
 	void update (branch_update *u, bool taken, unsigned int target) {
 		if (bi.br_flags & BR_CONDITIONAL) { // if conditional branch
-			perceptron& p = table[((my_update*)u)->index];
-			int output = p.predict(ghist);
+			int output = p.predict(bi.address, ghist);
 
 			if ((output >= 0) != taken || std::abs(output) <= THETA) {
-				p.train(ghist, taken);
+				p.train(bi.address, ghist, taken);
 			}
 			
 			ghist <<= 1;
